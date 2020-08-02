e698e3bef245769ee59faaeb064e8397eb4177e9
Davi Miranda Gonçalves <davi.mirandagonc@gmail.com>
2020-08-02 13:35:35 -0300

    V0.1

---
+++ scripts/popup.ctrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+document.getElementById("submit-cashout-manual").addEventListener("click", function () {
+    // document.getElementById("demo").innerHTML = "Hello World";
+    console.log($("#form-cashout-manual").serializeArray())
+    console.log($("#form-cashout-manual"))
+    // alert("$('#form-cashout-manual')", $("#form-cashout-manual").serializeArray());
+});
